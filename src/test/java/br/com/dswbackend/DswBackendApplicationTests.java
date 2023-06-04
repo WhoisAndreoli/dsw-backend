b7d44b22c8e866662c387ec4525c0925a9fbdf73
WhoisAndreoli <andre_oliiveira97@hotmail.com>
2023-06-04 18:51:30 -0300

    first commit

---
+++ src/test/java/br/com/dswbackend/DswBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dswbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
