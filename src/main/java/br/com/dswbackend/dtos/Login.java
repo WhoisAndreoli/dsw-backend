88997e6fba9d15d31ddc5e0d2e2e432a2b475d7e
WhoisAndreoli <andre_oliiveira97@hotmail.com>
2023-06-21 12:15:26 -0300

    update login changes

--- src/main/java/br/com/dswbackend/dtos/Login.java
+++ src/main/java/br/com/dswbackend/dtos/Login.java
@@ -1,10 +1,5 @@
 package br.com.dswbackend.dtos;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record Login(
-    @NotBlank(message = "O campo email deve ser preenchido") @Email(message = "Email com o formato inválido") String email,
-    String senha) {
+public record Login(String email, String senha) {
 
 }
